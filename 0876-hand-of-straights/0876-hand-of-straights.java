5e324576fc795c19e1dd0363539d1f57478abadc
gauthamkv07 <kvgautham010@gmail.com>
2024-06-05 20:50:46 -0400

    Time: 25 ms (88.26%), Space: 45.8 MB (19.65%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -3,22 +3,23 @@ class Solution {
         int n = hand.length;
         if(n % groupSize != 0) return false;
 
-        Map<Integer,Integer> map = new TreeMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int h: hand) map.put(h, map.getOrDefault(h,0) + 1);
 
-        for(int h: hand) map.put(h, map.getOrDefault(h, 0) + 1);
+        for(int card: hand) {
+            int curr = card;
+            while(map.getOrDefault(curr - 1, 0) > 0) curr--;
 
-        while(map.size() > 0) {
-            int curr = map.entrySet().iterator().next().getKey();
-
-            for(int i = 0; i < groupSize; i++) {
-                int key = curr + i;
-                if(!map.containsKey(key)) return false;
-
-                map.put(key, map.get(key) - 1);
-                if(map.get(key) == 0) map.remove(key);
-            }
+            while(curr <= card && map.getOrDefault(curr, 0) > 0) {
+                for(int i = curr; i < curr + groupSize; i++) {
+                    if(map.getOrDefault(i,0) == 0) return false;
+                    map.put(i, map.getOrDefault(i,0) - 1);
+                    if(map.get(i) == 0) map.remove(i);
+                }
+                if(!map.containsKey(curr)) curr++;
+            } 
         }
-
+        
         return true;
     }
 }

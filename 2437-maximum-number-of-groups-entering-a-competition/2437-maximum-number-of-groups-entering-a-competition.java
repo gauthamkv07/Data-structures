bc22341c7f53447dc5511bc66618d9f05f2bc940
gauthamkv07 <kvgautham010@gmail.com>
2024-06-12 23:32:24 -0400

    Time: 0 ms (100%), Space: 57.5 MB (90.73%) - LeetHub

--- 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.java
+++ 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.java
@@ -1,18 +1,5 @@
 class Solution {
     public int maximumGroups(int[] grades) {
-        Arrays.sort(grades);
-        int psum = grades[0],sum = 0, pgs = 1,cgs = 0, res = 1;
-        for(int i = 1; i < grades.length; i++) {
-            sum += grades[i];
-            cgs++;
-            if(cgs > pgs && sum > psum) {
-                res++;
-                psum = sum;
-                sum = 0;
-                pgs = cgs;
-                cgs = 0;
-            }
-        }
-        return res;
+        return (int)((Math.sqrt(grades.length * 8 + 1) - 1)/2);
     }
 }

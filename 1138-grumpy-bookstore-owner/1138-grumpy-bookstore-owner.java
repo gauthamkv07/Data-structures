ea64ac3f4790e887681c7b9e3f51fa93c2b98114
gauthamkv07 <kvgautham010@gmail.com>
2024-06-20 21:28:16 -0400

    Time: 109 ms (11.98%), Space: 48.3 MB (7.12%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int m) {
-        int n = customers.length, ans = 0;
-        for(int i = 0; i < n; i++) if(grumpy[i] == 0) ans += customers[i];
-
-        int max = 0, curr = 0;
-        for(int i = 0; i < m; i++) if(grumpy[i] == 1) curr += customers[i];
-        max = curr;
-        for(int i = m; i < n; i++) {
-            if(grumpy[i] == 1) curr += customers[i];
-            if(grumpy[i-m] == 1) curr -= customers[i-m];
+        int n = customers.length, ans = 0, max = 0, curr = 0;
+        for(int i = 0; i < n; i++) {
+            if(i-m >= 0 && grumpy[i-m] == 1) curr -= customers[i-m];
+            if(grumpy[i] == 0) {
+                ans += customers[i];
+                continue;
+            }
+            curr += customers[i];
+            System.out.println(curr + " " + i);
             max = Math.max(curr, max);
         }
+
 
         return ans + max;
     }

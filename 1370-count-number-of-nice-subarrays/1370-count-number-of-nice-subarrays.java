9a2039fd6f4521cdc3d74b5f08c123b0e02e6114
gauthamkv07 <kvgautham010@gmail.com>
2024-06-22 08:51:01 -0400

    Time: 7 ms (96.52%), Space: 54.6 MB (88.96%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,13 +1,16 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        
-        int lastPop = -1, n = nums.length, res = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] % 2 == 1) q.offer(i);
-            if(q.size() > k) lastPop = q.poll();
-            if(q.size() < k) continue;
-            res += q.element() - lastPop;
+        int iniGap = 0, cur = 0,res = 0, n = nums.length;
+        for(int i = 0, j = 0; i < n; i++) {
+            if(nums[i] % 2 == 1) cur++;
+            if(cur == k) {
+                iniGap = 0;
+                while(cur == k) {
+                    iniGap++;
+                    if(nums[j++] % 2 == 1) cur--;
+                }
+            }
+            res += iniGap;
         }
         return res;
     }

1201f67b8da1aa43725457eff18677299d0aa395
gauthamkv07 <kvgautham010@gmail.com>
2024-07-07 20:48:46 -0400

    Time: 0 ms (100%), Space: 40.5 MB (50.17%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,39 +1,7 @@
 class Solution {
-    class ListNode{
-        int val;
-        ListNode next;
-
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
-
     public int findTheWinner(int n, int k) {
-        ListNode prev = null, head = null;
-        for(int i = 1; i <= n; i++) {
-            ListNode curr = new ListNode(i);
-            if(i != 1) {
-                prev.next = curr;
-                prev = prev.next;
-            } else {
-                prev = curr;
-                head = curr;
-            }
-        }
-        prev.next = head;
-        int i = 0;
-        while(n > 1) {
-            i++;
-            if(i == k) {
-                i = 0;
-                n--;
-                prev.next = prev.next.next;
-                head = prev.next;
-            } else {
-                prev = head;
-                head = head.next;
-            }
-        }
-        return head.val;
+        int ans = 0;
+        for(int i = 2; i <= n; i++) ans = (ans + k) % i;
+        return ans + 1;
     }
 }

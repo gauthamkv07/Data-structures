ae65c022513cab7cfb65b461290f6b028a73a145
gauthamkv07 <kvgautham010@gmail.com>
2024-08-21 01:43:13 -0400

    Time: 36 ms (13.88%), Space: 43.4 MB (44.49%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -2,16 +2,16 @@ class Solution {
     public int strangePrinter(String s) {
         int n = s.length();
         int[][] dp = new int[n][n];
-        for(int len = 2; len <=n; len++){
-            for(int l = 0; l <= n-len; l++) {
-                int r = l + len - 1;
-                int j = -1;
+
+        for(int len  = 2; len <= n; len++) {
+            for(int l = 0; l <= n - len; l++) {
+                int r = l + len - 1, j = -1;
                 dp[l][r] = n;
                 for(int i = l; i < r; i++) {
-                    if(s.charAt(i) != s.charAt(r) && j==-1) j =i;
+                    if(s.charAt(i) != s.charAt(r) && j == -1) j = i;
                     if(j != -1) dp[l][r] = Math.min(dp[l][r], 1 + dp[j][i] + dp[i+1][r]);
                 }
-                if(j == -1) dp[l][r] = 0; 
+                if(j == -1) dp[l][r] = 0;
             }
         }
 

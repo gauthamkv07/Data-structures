eb476fc49c214b15200e9a5e245d2e4b8682dc74
gauthamkv07 <kvgautham010@gmail.com>
2024-09-26 00:43:34 -0400

    Time: 23 ms (53.39%), Space: 45.4 MB (53.74%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -7,16 +7,16 @@ class MyCalendar {
     }
     
     public boolean book(int start, int end) {
-        Integer prev = calendar.floorKey(start),
-                next = calendar.ceilingKey(start);
-        if ((prev == null || calendar.get(prev) <= start) &&
-                (next == null || end <= next)) {
+        Integer prev = calendar.floorKey(start), next = calendar.ceilingKey(start);
+
+        if((prev == null || calendar.get(prev) <= start) && (next == null || end <= next)) {
             calendar.put(start, end);
             return true;
         }
         return false;
     }
 }
+
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar obj = new MyCalendar();
